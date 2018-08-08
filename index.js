ad2fd53a34ce6c2918930ad8c27b789252406b31
Nikhil Ranjan <niklabh811@gmail.com>
2018-08-08 15:11:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var Peer = require('simple-peer')
+var p = new Peer({ initiator: location.hash === '#1', trickle: false })
+
+p.on('error', function (err) { console.log('error', err) })
+
+p.on('signal', function (data) {
+  console.log('SIGNAL', JSON.stringify(data))
+  document.querySelector('#outgoing').textContent = JSON.stringify(data)
+})
+
+document.querySelector('form#signal').addEventListener('submit', function (ev) {
+  ev.preventDefault()
+  p.signal(JSON.parse(document.querySelector('#incoming').value))
+})
+
+p.on('connect', function () {
+  console.log('CONNECT')
+  p.send('whatever' + Math.random())
+
+  document.querySelector('form#message').addEventListener('submit', function (ev) {
+    ev.preventDefault()
+    p.send(document.querySelector('#outMsg').value)
+  })
+})
+
+p.on('data', function (data) {
+  document.querySelector('#inMsg').innerHTML += `<li>${data}</li>`
+  console.log('data: ' + data)
+})
